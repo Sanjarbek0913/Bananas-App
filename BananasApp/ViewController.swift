06eab37208f7fac7dc10e5ed61186add34ca9cd4
Sanjarbek <sanjarbekabdurayimov4@gmail.com>
2023-07-02 08:15:06 +0500

    Initial Commit

---
+++ BananasApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BananasApp
+//
+//  Created by Sanjarbek Abdurayimov on 02/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
